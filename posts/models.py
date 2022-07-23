448f1c134e57484090418135222dca5e7a81cbff
Shaygan Askari <shaygan.askari@gmail.com>
2022-07-23 17:16:39 +0430

    style: Visual Update
    Add Logo to post model
    Update the visuals of index.html and base.html templates
    Fix minor syntax issue of index.html
    Register post model to admin site

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 
 from django.contrib.auth.models import User
@@ -7,6 +8,7 @@ class Post(models.Model):
 
     title = models.CharField(max_length=75)
     body = models.TextField()
+    logo = models.ImageField(upload_to=f'user_data/', default='static/placeholder.png')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
