60e81c68d8f4980abf9724b9d0950330ab29890f
Shaygan Askari <shaygan.askari@gmail.com>
2022-08-12 22:25:35 +0430

    fix: Fix the media and static files and post logo
    update gitignore to ignore user data
    add MEDIA_URL, MEDIA_ROOT, STATICFILES_DIRS to settings.py
    add media url to urls.py
    create and migrate post publish date
    reformat index and base templates for for better visuals
    move static folder to the main dir

--- posts/models.py
+++ posts/models.py
@@ -1,21 +1,21 @@
 from django.db import models
-
 from django.contrib.auth.models import User
+
+from datetime import datetime
+
+def get_file_path(instance, filename):
+    file_extension = filename.split('.')[-1]
+    return f'user_{instance.author}/{instance.pub_date.strftime("%Y/%B/%a-%M")}/logo.{file_extension}'
 
 
 class Post(models.Model):
 
     title = models.CharField(max_length=75)
     body = models.TextField()
-    logo = models.ImageField(upload_to=f'user_data/', default='static/placeholder.png')
+    logo = models.ImageField(upload_to=get_file_path, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(default=datetime.now())
 
 
     def __str__(self):
         return str(self.author) + ' | ' + self.title
-
-    def get_short_body(self):
-        if len(self.body) > 50:
-            return self.body[:50] + '...'
-        else:
-            return self.body
