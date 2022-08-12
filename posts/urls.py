ad37235dd87c9c4a4a995d75c9fa0fd854bbf5bf
Shaygan Askari <shaygan.askari@gmail.com>
2022-08-12 23:09:09 +0430

    feat: Add details page
    update the navbar size to small
    add login button(non-functional) to navbar
    create a url for details page

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,5 @@ from .views import PostListView, PostDetailView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='index'),
-    path('<int:pk>/', PostDetailView.as_view(), name='detail'),
+    path('<int:pk>/', PostDetailView.as_view(), name='details'),
 ]
