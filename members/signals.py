fe4c9419ebd804573d65a71bbaa46aa619b6c505
Shaygan <shaygan.askari@gmail.com>
2022-10-24 14:24:13 +0330

    fix + visual: Fix profile slug signal and change visuals of profile page
    profile's slug changes with changing username now
    removed extra spaces from bottom of post list in profile page
    added extra spaces at the bottom of the page

--- members/signals.py
+++ members/signals.py
@@ -8,15 +8,18 @@ from .models import Profile
 User = get_user_model()
 
 @receiver(pre_save, sender=User)
-def create_user(sender, instance, **kwargs):
+def update_user(sender, instance, **kwargs):
     """Sets user's slug to slugified user's username before saving the user"""
     instance.slug = slugify(instance.username)
 
 
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
+def create_update_profile(sender, instance, created, **kwargs):
     """Creates a profile for user and set's it's slug to slugified user's username only on user creation"""
 
-    #TODO: Check if this fails when changing username
     if created:
         Profile.objects.create(user=instance, slug=instance.slug)
+    
+    elif instance.profile.slug != instance.slug:
+        instance.profile.slug = instance.slug
+        instance.profile.save()
